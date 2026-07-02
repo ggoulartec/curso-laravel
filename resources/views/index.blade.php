938f67812e9f74280ad9dc7dfd8e00ff95c7f640
goularti <sistemas4@leiloesjudiciais.com.br>
2026-07-02 10:09:55 -0300

    :sparkles: feat: setup tailwind

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,14 @@
-{{--@dd($name, $habits)--}}
-<h1>Welcome to home page</h1>
-<p>
-    Olá {{ $name }}
-</p>
-<p>
-    Seus Hábitos são:
-</p>
-<ul>
-    @foreach($habits as $habit)
-        <li>{{$habit}}</li>
-    @endforeach
-</ul>
-
+<!doctype html>
+<html lang="pt_br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>{{config('app.name')}}</title>
+  @vite('resources/css/app.css')
+</head>
+<body>
+<h1 class="text-2xl font-bold">Hello world!</h1>
+</body>
+</html>
